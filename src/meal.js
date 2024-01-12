e82bcdfa02947847fc04d13d046102743492be7a
Julian Beldotti <julian.beldotti@gmail.com>
2024-01-11 23:15:52 -0700

    Fix: . Now add the files changed including the completing of warm-up

--- src/meal.js
+++ src/meal.js
@@ -1,13 +1,31 @@
-function nameMenuItem() {
-
+function nameMenuItem(foodName) {
+  return `Delicious ${foodName}`;
 }
 
+function createMenuItem(name, price, type) {
+  var menuItem = {
+    name: name,
+    price: price,
+    type: type
+  }
+  return menuItem;
+}
+
+function addIngredients(food, arr) {
+  if (!arr.includes(food)) {
+    arr.push(food);
+  }
+}
+
+function formatPrice(price) {
+  return "$" + price;
+}
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
+  createMenuItem,
+  addIngredients,
+  formatPrice,
   // decreasePrice,
   // createRecipe
 }
