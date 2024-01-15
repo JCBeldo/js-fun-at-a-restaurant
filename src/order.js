28205fb1262d0d802152dcab59a0f08014770d1c
Julian Beldotti <julian.beldotti@gmail.com>
2024-01-15 08:09:01 -0700

    Feat: Make the code pass the tests

--- src/order.js
+++ src/order.js
@@ -1,7 +1,42 @@
+function takeOrder(order, arr) {
+  if(arr.length >= 3) {
+    return arr;
+  }
+  else {
+    arr.push(order);
+    return arr;
+  }
+}
+
+function refundOrder(orderNum, arr) {
+  for ( let i = 0; i < arr.length; i++) {
+    while(arr[i].orderNumber === orderNum) {
+      arr.splice(i,1); // use i instead of arr[i] for index #
+    }
+  }
+  return arr;
+}
+
+function listItems(arr) {
+  items = []
+  for (let i = 0; i < arr.length; i++) {
+  items.push(arr[i].item);
+  }
+  return items.join(", ");
+}
+
+function searchOrder(arr, str) {
+  for (i = 0; i < arr.length; i++) {
+    if (arr[i].item === str || arr[i].item.includes(str)) {
+      return true
+    }
+  }
+  return false //instead of else, just return outside of for, b/c return breaks
+}
 
 module.exports = {
-  // takeOrder,
-  // refundOrder,
-  // listItems,
-  // searchOrder
+  takeOrder,
+  refundOrder,
+  listItems,
+  searchOrder
 }
